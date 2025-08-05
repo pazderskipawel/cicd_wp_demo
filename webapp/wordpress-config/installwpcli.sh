bfa75d5629a58b1cf1dc9aa7344cbbe71bddb994
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-08-05 19:34:19 +0200

    cicd improvements, moved sh scripts to workflow, created workflow for troubleshooting

--- webapp/wordpress-config/installwpcli.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install wp-cli in the WordPress container
-curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && 
-chmod +x wp-cli.phar && 
-mv wp-cli.phar /usr/local/bin/wp
-
