f3840ffa04179ebc30e4ac4996180e06598e138a
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-08-28 16:55:53 +0200

    removed script and put command directly

--- webapp/scripts/trigger-wp-backup.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-curl -X POST \
-  -H "Authorization: token $GITHUB_TOKEN" \
-  -H "Accept: application/vnd.github.v3+json" \
-  https://api.github.com/repos/$GITHUB_REPO/actions/workflows/app_save_wp_backup.yml/dispatches
