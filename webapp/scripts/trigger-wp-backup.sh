858b98fd94ea3110bc83ce9f8dce7b88d19adc13
Pawel Pazderski <pawelpazderski@hotmail.com>
2025-08-28 16:17:07 +0200

    added auto trigger of backup when containers stops, added restoring from backup in main pipeline

---
+++ webapp/scripts/trigger-wp-backup.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+curl -X POST \
+  -H "Authorization: token $GITHUB_TOKEN" \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/$GITHUB_REPO/actions/workflows/app_save_wp_backup.yml/dispatches
